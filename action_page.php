5d0dd402fece8cdc2c74deb06ed4a6f0139465b3
Connor McDermid <cam-o-man@live.com>
2019-10-03 18:13:15 -0400

    Fixing file uploads
    Signed-off-by: Connor McDermid <cam-o-man@live.com>

--- action_page.php
+++ action_page.php
@@ -4,12 +4,36 @@
         <meta name="description" content="You have successfully submitted your file."
         <title>Submitted</title>
         <!-- handle file upload -->
+
         <?php
             $target_dir = "/uploads";
             $target_file = $target_dir . basename($_FILES["file"]["name"]);
             $uploadOK = 1;
             $fileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
         ?>
+
+      <?php
+        $name = ($_POST["firstname"] + $_POST["lastname"]);
+        mkdir("uploads/" . $name);
+        $target_dir = "uploads/" . $name;
+        $uploadOK = 1;
+
+        if (file_exists($target_file)) {
+          echo "Sorry, that file already exists.";
+          $uploadOK = 0;
+        }
+
+        if ($uploadOK === 0) {
+          echo "Sorry, there has been an error uploading your file.";
+        } else {
+          if (move_uploaded_file($_FILES["file"]["name"], $target_file)) {
+            echo "The file ". basename( $_FILES["file"]["name"]). " has been uploaded";
+          } else {
+            echo "An error has been encountered.";
+          }
+        }
+      ?>
+
     </head>
     <body>
     <!-- All content below here-->
