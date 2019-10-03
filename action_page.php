c3289c683d00a03fd2ab9cdf922e807ace5f3101
Connor <cam-o-man@live.com>
2019-10-03 17:15:19 -0400

    Committing files from my laptop
    Signed-off-by: Connor <cam-o-man@live.com>

---
+++ action_page.php
@@ -0,0 +1,23 @@
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="description" content="You have successfully submitted your file."
+        <title>Submitted</title>
+        <!-- handle file upload -->
+        <?php
+            $target_dir = "/uploads";
+            $target_file = $target_dir . basename($_FILES["file"]["name"]);
+            $uploadOK = 1;
+            $imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
+
+
+        ?>
+    </head>
+    <body>
+    <!-- All content below here-->
+    <?php echo $_POST["firstname"]; ?> <?php echo $_POST["lastname"]; ?> from <?php echo $_POST["academy"]; ?>, you <br/>
+    have successfully submitted
+
+    <!-- All content above here-->
+    </body>
+</html>
